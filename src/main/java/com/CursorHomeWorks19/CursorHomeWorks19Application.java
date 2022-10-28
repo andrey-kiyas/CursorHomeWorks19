c68386882d1938ea176112c11acac41387177880
ANDREY <andrey.kiyas@gmail.com>
2022-10-28 14:16:54 +0300

    PR01 "Homework 19"

---
+++ src/main/java/com/CursorHomeWorks19/CursorHomeWorks19Application.java
@@ -0,0 +1,13 @@
+package com.CursorHomeWorks19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorHomeWorks19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursorHomeWorks19Application.class, args);
+    }
+
+}
